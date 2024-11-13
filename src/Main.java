5ecd5cd5c4e9520930fd359dcadcb47266ed5fc4
stazeal <peacestazeal@gmail.com>
2024-11-13 06:59:59 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+
+
+//public  class Main {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+
+//    }
+//}
+
+
+//variable the container that contain the value
+public  class Main {
+    public static  void main(String[] args)
+    {
+// how variable is applied
+
+        int  age = 22;
+        String name = "Anna Amani Mwampulule";
+
+        System.out.println(age);
+        System.out.println(name);
+//concatinations of word that is String  concatination
+        System.out.println( "The name is "+ age  +" "+  name);
+
+
+
+//      if statement in java programming
+          int marks = 70;
+
+          if( marks>60) {
+              System.out.println("The greatest value");
+
+          }
+          else {
+              System.out.println("Smallest");
+          }
+          System.out.println(marks);
+
+
+    }
+}
+
