1a27903bb0ffa02db6e1249db1a3d122a0210535
stazeal <peacestazeal@gmail.com>
2024-11-13 18:08:42 +0300

    addition of arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
-
-
-
+import java.util.ArrayList;
 
 //variable the container that contain the value
 public  class Main {
     public static void main(String[] args) {
-//        Array and ArrayList
-     String jina_la_kwanza = "filbati";
-     String jina_la_pili = "Anna";
+//
 
-     String[]majina = {"filbati", "Anna"};
-
-     System.out.println(majina[1]);
+//     Another array that change called ArrayList
 
 
-}}
+
+
+                ArrayList<String> matunda = new ArrayList<>();
+                matunda.add("Filbati");
+                matunda.add("Anna");
+                matunda.add("Amani");
+
+                // Accessing the first element
+                System.out.println(matunda.get(0));  // Outputs: Filbati
+                // Printing the size of the array list
+                System.out.println(matunda.size());  // Outputs: 3
+
+                // Optional return statement, not necessary here
+           matunda.remove(0);
+            }
+        }
