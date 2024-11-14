53da6c51eacb5291d0571d1ac47d9283c919a717
stazeal <peacestazeal@gmail.com>
2024-11-15 02:22:21 +0300

    addition of arraylist

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,24 @@ public  class Main {
 //     Another array that change called ArrayList
 
 
+//
+//
+//                ArrayList<String> matunda = new ArrayList<>();
+//                matunda.add("Filbati");
+//                matunda.add("Anna");
+//                matunda.add("Amani");
+//
+//                // Accessing the first element
+//                System.out.println(matunda.get(0));  // Outputs: Filbati
+//                // Printing the size of the array list
+//                System.out.println(matunda.size());  // Outputs: 3
+//
+//                // Optional return statement, not necessary here
+//           matunda.remove(0);
+        int[]marks = {20,45,46,30,20};
+        System.out.println(marks[0]);
 
 
-                ArrayList<String> matunda = new ArrayList<>();
-                matunda.add("Filbati");
-                matunda.add("Anna");
-                matunda.add("Amani");
 
-                // Accessing the first element
-                System.out.println(matunda.get(0));  // Outputs: Filbati
-                // Printing the size of the array list
-                System.out.println(matunda.size());  // Outputs: 3
-
-                // Optional return statement, not necessary here
-           matunda.remove(0);
             }
         }
