0f77338643db1631f00c15fe4f75296a93df6787
stazeal <peacestazeal@gmail.com>
2024-11-15 02:59:17 +0300

    Addition of  foreach loop

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,11 @@ public  class Main {
     public static void main(String[] args) {
 
         int[]marks = {20,45,46,30,20};
-//      Do  While group
-//        initial condition = 0
-//        change condition = increment (Ongeza)
-//        Test condition = (==4)
-        int i=0;
-       do{
-            System.out.println( "marks"+marks[i]);
-            i++;
+//      For each loop
 
-        }
-        while (i<=5);
-
+       for(int mark:marks) {
+           System.out.println(mark);
+       }
 
 
 
