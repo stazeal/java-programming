c81ac6a114c2c900df34cfdd32b3c22b5b73900d
stazeal <peacestazeal@gmail.com>
2024-11-15 02:43:48 +0300

    Addition of while group

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public  class Main {
     public static void main(String[] args) {
 
         int[]marks = {20,45,46,30,20};
-//        For loop
+//        While group
 //        initial condition = 0
 //        change condition = increment (Ongeza)
 //        Test condition = (==4)
-        for (int i=4; i>0; i--){
+        int i=0;
+        while (i<=5){
             System.out.println( "marks"+marks[i]);
+            i++;
 
         }
 
