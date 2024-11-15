f6c701bfc8c2f7aa2167fc05d27b4daaa0d54a93
stazeal <peacestazeal@gmail.com>
2024-11-15 03:19:48 +0300

    Addition of classes and object

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,10 @@
 public  class Main {
     public static void main(String[] args) {
 
-        int[]marks = {20,45,46,30,20};
-//      For each loop
-
-       for(int mark:marks) {
-           System.out.println(mark);
-       }
-
-
-
-
-            }
+//        Classes
+       Mtu m = new Mtu();
+       m.jina = "Anna";
+       m.umri =24;
+       m.salimia();
+    }
         }
---
+++ src/Mtu.java
@@ -0,0 +1,9 @@
+public class Mtu {
+
+    int umri;
+    String jina;
+
+    public  void salimia(){
+        System.out.println("Habari! Jina langu ni "+jina +"nina miaka"+umri);
+    }
+}
