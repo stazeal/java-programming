a7d8ebce7f2019521cf668c3cbeb40abba5d4bdf
stazeal <peacestazeal@gmail.com>
2024-11-16 05:05:33 +0300

    class and object in another way

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,15 @@
 public  class Main {
     public static void main(String[] args) {
 
-//        Classes
-       Mtu m = new Mtu();
-       m.jina = "filly";
-       m.umri =24;
-       m.salimia();
+//      Object in the main class
+//       Mtu m = new Mtu();
+//       m.jina = "filly";
+//       m.umri =24;
+//
+
+//        Another to create object
+        Mtu mtu = new Mtu( 23, "Anna");
+        mtu.salimia();
+
     }
         }
--- src/Mtu.java
+++ src/Mtu.java
@@ -3,6 +3,12 @@ public class Mtu {
     int umri;
     String jina;
 
+
+    Mtu(int umri, String jina){
+        this.umri = umri;
+        this.jina = jina;
+    }
+
     public  void salimia(){
         System.out.println("Habari! Jina langu ni "+jina +"nina miaka"+umri);
     }
