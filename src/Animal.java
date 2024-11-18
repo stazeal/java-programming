dfc88bd72ef0f88481b90f6ed99c0acddd13a3b0
stazeal <peacestazeal@gmail.com>
2024-11-18 10:46:20 +0300

    polymorphism

--- src/Animal.java
+++ src/Animal.java
@@ -2,11 +2,22 @@ public class Animal {
     String name;
     String color;
     int age;
+    String wings;
     int numberOfLegs;
 
-
     public void move(){
-        System.out.println("Animal is moving with "+ numberOfLegs + " legs"+ "the animal is"+name);
+        System.out.println("Animal is moving");
     }
 
+    public void move(int numberOfLegs){
+        System.out.println("am moving with " + numberOfLegs + " legs");
+    }
+
+    public void move(String name){
+        System.out.println("my name is " + name);
+    }
+
+    public void moveWithWings(String wings){
+        System.out.println("my wings are " + wings);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,16 @@ public  class Main {
 
 
         Cat cat =new Cat();
-            cat.name ="cat one";
+            cat.name ="cat";
             cat.color = "purple";
             cat.numberOfLegs = 2;
             cat.age = 23;
+            cat.wings ="Two wings";
 
             cat.move();
+        cat.move(6);
+        cat.move(cat.name);
+
 
 
     }
